c41e11f3efa074a3e7b1a5dc944a5f6b2f625bcc
as <aslak.bakkeland@medialaben.no>
2024-02-23 19:38:53 +0100

    add kasper due to getSrcSetElement

--- targets.js
+++ targets.js
@@ -1,6 +1,9 @@
 const puppeteer = require("puppeteer");
 
-const targets = ["https://dagbladet--www-integration.labdevs.com"];
+const targets = [
+  "https://dagbladet--www-integration.labdevs.com",
+  "https://image-www-kasper.labdevs.com",
+];
 
 targets.forEach((tar) => {
   (async () => {
