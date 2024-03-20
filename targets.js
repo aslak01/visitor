eef0d81cab42497b4c81083bdbb95e5c859a4fc1
as <aslak.bakkeland@medialaben.no>
2024-03-20 15:46:09 +0100

    remove redundant visits

--- targets.js
+++ targets.js
@@ -1,8 +1,6 @@
 const puppeteer = require("puppeteer");
 
 const targets = [
-  "https://dagbladet--www-integration.labdevs.com",
-  "https://image-www-kasper.labdevs.com",
   "https://dagbladet--www-aslak.labdevs.com/skatt"
 ];
 
