40357c94490e6f5b204c113a34c2a517f47b5175
as <aslak.bakkeland@medialaben.no>
2024-03-20 15:45:42 +0100

    add my cluster

--- targets.js
+++ targets.js
@@ -3,6 +3,7 @@ const puppeteer = require("puppeteer");
 const targets = [
   "https://dagbladet--www-integration.labdevs.com",
   "https://image-www-kasper.labdevs.com",
+  "https://dagbladet--www-aslak.labdevs.com/skatt"
 ];
 
 targets.forEach((tar) => {
